d016cf051234b182f76154e53831fd27c129b973
ing-arriola <ingeniero.arriola@gmail.com>
2023-10-05 15:28:06 -0400

    refactor DB connection

--- scripts/loadFakeData.ts
+++ scripts/loadFakeData.ts
@@ -1,22 +1,13 @@
-import { Client } from "pg"
-import { loadEnvConfig } from "@next/env";
 import { faker } from '@faker-js/faker'
 import  bcrypt  from 'bcrypt'
+import { getClient } from "@/db";
 
-const projectDir = process.cwd();
 
-loadEnvConfig(projectDir);
 
 const loadFakeData = async (numUsers:number) => {
     console.log('executing loadFakeData....')
-    console.log(`${process.env.POSTGRES_USER} ${process.env.POSTGRES_HOST} ${process.env.POSTGRES_NAME} ${process.env.POSTGRES_PASSWORD} ${process.env.POSTGRES_PORT!}`)
-    const client = new Client({
-        user:process.env.POSTGRES_USER,
-        host:process.env.POSTGRES_HOST,
-        database:process.env.POSTGRES_NAME,
-        password:process.env.POSTGRES_PASSWORD,
-        port: parseInt(process.env.POSTGRES_PORT!),
-    })
+
+    const client = await getClient()
 
     await client.connect()
 
